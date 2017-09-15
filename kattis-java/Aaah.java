2e97211d48370aca7bc990259b8e1390ee0ca13c
soer7022 <soer7033@gmail.com>
2017-09-15 22:57:15 +0200

    Solved problem: Aaah!

---
+++ kattis-java/Aaah.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Aaah {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String mariusAaah = sc.next();
+        String doctorAaah = sc.next();
+
+        if(mariusAaah.length() >= doctorAaah.length()) {
+            System.out.println("go");
+        }
+        else {
+            System.out.println("no");
+        }
+
+    }
+}
