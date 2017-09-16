1cfa7b667198e695a188f6e0376a518c3273d97d
soer7022 <soer7033@gmail.com>
2017-09-16 02:03:28 +0200

    Solved dicegame

---
+++ kattis-java/Solved/D/dicegame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class dicegame {
+    public static void main(String[] args) {
+        int gunnarTotal = 0;
+        int emmaTotal= 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; i <= 4; i++) {
+            gunnarTotal += sc.nextInt();
+        }
+        for (int i = 1; i <= 4; i++) {
+            emmaTotal += sc.nextInt();
+        }
+        if(gunnarTotal > emmaTotal) {
+            System.out.println("Gunnar");
+        }
+        if (gunnarTotal < emmaTotal) {
+            System.out.println("Emma");
+        }
+        if (gunnarTotal == emmaTotal) {
+            System.out.println("Tie");
+        }
+    }
+}
