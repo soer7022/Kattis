a10a8e56d5cf600ce436cc6dc32fd82e72c74e83
soer7022 <soer7033@gmail.com>
2017-09-17 03:08:31 +0200

    Solved problem: TwoStones

---
+++ kattis-java/Solved/T/twostones.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class twostones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int stones = sc.nextInt();
+        if(stones % 2 == 0) {
+            System.out.println("Bob");
+        }
+        else {
+            System.out.println("Alice");
+        }
+    }
+}
