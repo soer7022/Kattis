17f9c39fcb020e01f9cb754cfb6eac484ae2c801
soer7022 <soer7033@gmail.com>
2017-10-20 22:31:38 +0200

    Solved What Does The Fox Say?

--- kattis-java/Solved/W/WhatDoesTheFoxSay/wdtfs.java
+++ kattis-java/Solved/W/WhatDoesTheFoxSay/wdtfs.java
@@ -1,20 +1,30 @@
-package W.WhatDoesTheFoxSay;
+package Solved.W.WhatDoesTheFoxSay;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class wdtfs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i <= n; i++) {
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
             ArrayList<String> fox = new ArrayList<>();
             String input = sc.nextLine();
             String[] output = input.split(" ");
-            for(String s : output) {
-                fox.add(s);
+            Collections.addAll(fox, output);
+            String currentLine = "";
+            while (!currentLine.contains("fox")) {
+                currentLine = sc.nextLine();
+                String wordToRemove = currentLine.substring(currentLine.indexOf("goes") + 5, currentLine.length());
+                fox.removeAll(Collections.singleton(wordToRemove));
             }
+            for (String s : fox) {
+                System.out.print(s + " ");
+            }
+            System.out.println("");
         }
-        while(String Object currentLine;currentLine)
+
     }
 }
