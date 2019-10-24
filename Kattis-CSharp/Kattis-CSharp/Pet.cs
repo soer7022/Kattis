ead2967333a726cc8b88d9012ccc277e048d9e33
Søren Eriksen <soeren.skov.eriksen@lego.com>
2019-10-24 10:18:17 +0200

    add CSharp

---
+++ Kattis-CSharp/Kattis-CSharp/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Kattis_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int highestScore = 0;
+            int currentParticipant = 0;
+            int bestParticipant = 0;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] scores = line.Split(" ");
+                currentParticipant++;
+                int sum = 0;
+                foreach (string score in scores)
+                {
+                    sum += Int32.Parse(score);
+                }
+                if (sum > highestScore)
+                {  
+                    highestScore = sum;
+                    bestParticipant = currentParticipant;
+                }
+            }
+            Console.WriteLine( bestParticipant + " " + highestScore);
+        }
+    }
+}
